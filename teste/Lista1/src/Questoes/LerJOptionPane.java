fb3f50f9dc179c3b7e8541135559f171034e9a73
Águida Ariane <aguida.s@aluno.ifsc.edu.br>
2023-02-28 09:10:33 -0300

    LISTA 1 28/02

---
+++ teste/Lista1/src/Questoes/LerJOptionPane.java
@@ -0,0 +1,13 @@
+package Questoes;
+
+
+public class LerJOptionPane {
+    
+	public static void main(String[] args) {
+		Integer valor = JOptionPane.ShowInputDialog("valor1, valor2");
+		 
+		Integer quantlitros;
+		
+		
+	}
+}
